7eb3a5fcb5976ee9e9c95968058c6b57b04e17e0
Sylvain Boily <sboily@avencall.com>
2015-12-27 12:27:34 -0500

    re-added stasis incoming room

--- xivo_ctid_ng/plugins/incoming_room/stasis.py
+++ xivo_ctid_ng/plugins/incoming_room/stasis.py
@@ -18,4 +18,38 @@ class IncomingRoomCallsStasis(object):
         self.services = services
 
     def subscribe(self):
-        pass
+        self.ari.on_channel_event('StasisStart', self.join_incoming_call)
+        self.ari.on_channel_event('StasisEnd', self.leave_incoming_call)
+
+    def join_incoming_call(self, event_objects, event):
+        if event:
+            args = event.get('args')
+ 
+        if args[0] == 'dialed_from' or args[0] == 'blindtransfer':
+            return
+
+        channel = event_objects['channel']
+        instance_name = args[0].strip()
+        application_name = event.get('application')
+        incoming_room_id = "{}.{}".format(application_name, instance_name)
+
+        bridge_id = self.ari.asterisk.getGlobalVar(variable=incoming_room_id).get('value', None)
+
+        if bridge_id:
+            bridge = self.ari.bridges.get(bridgeId=bridge_id)
+            if len(bridge.json.get('channels')) < 1:
+                bridge.startMoh()
+        else:
+            bridge = self.ari.bridges.create(type='holding')
+            bridge_id = bridge.id
+            self.ari.asterisk.setGlobalVar(variable=incoming_room_id, value=bridge_id)
+            bridge.startMoh()
+        channel.answer()
+        bridge.addChannel(bridgeId=bridge_id, channel=channel.id)
+
+        bus_event = JoinCallIncomingRoomEvent({})
+        self.bus.publish(bus_event)
+
+    def leave_incoming_call(self, event_objects, event):
+        bus_event = LeaveCallIncomingRoomEvent({})
+        self.bus.publish(bus_event)
